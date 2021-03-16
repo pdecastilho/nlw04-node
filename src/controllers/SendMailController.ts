2ae14330495365b4c6a5fd5c9c61a08eb23a3c51
Pedro Castilho <pedro@castilho.in>
2021-03-16 14:15:21 -0300

    Day 5 - Calculate NPS and set user data validation

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -26,21 +26,23 @@ class SendMailController {
       });
     }
 
+    const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs");
+
+    const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
+      where: {user_id: user.id, value: null},
+      relations: ["user", "survey"],
+    });
+    
     const variables = {
       name: user.name,
       title: survey.title,
       description: survey.description,
-      user_id: user.id,
+      id: "",
       link: process.env.URL_MAIL,
     }
-
-    const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs");
-
-    const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
-      where: [{user_id: user.id}, {value: null}],
-      relations: ["user", "survey"],
-    });
+    
     if(surveyUserAlreadyExists) {
+      variables.id = surveyUserAlreadyExists.id;
       await SendMailService.execute(email, survey.title, variables, npsPath);
       return response.status(200).json(surveyUserAlreadyExists);
     }
@@ -50,6 +52,8 @@ class SendMailController {
       survey_id
     })
     await surveysUsersRepository.save(surveyUser);
+
+    variables.id = surveyUser.id;
 
     await SendMailService.execute(email, survey.title, variables, npsPath);
     
